04cfcca3f6a1be0ad1f0eafb4884d0c365bf9146
Jordan Lancheros <jordan1703@hotmail.es>
2021-01-31 22:19:16 -0500

    fix router

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :scrapping_bet, ScrappingBet.Repo,
   username: "postgres",
   password: "postgres",
   database: "scrapping_bet_dev",
-  hostname: "db",
+  hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
